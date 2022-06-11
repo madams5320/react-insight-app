9aebf398e0a5a0f97dde612be89c2c2c51335e52
RoseGolden5320 <makela.adams@gmail.com>
2022-06-11 19:57:30 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Homepage from './pages/Home';
-import React, {useState} from 'react'
-import Continents from './pages/Continents'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import pages
+import Home from './pages/Home';
+import Continents from "./pages/Continents";
+import Countries from "./pages/Countries";
+import Country from "./pages/Country";
 
 function App() {
   return (
-    <>
-      <Homepage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="continents" element={<Continents />} />
+          <Route path="countries">
+            <Route index element={<Countries />} />
+            <Route path=":countryId" element={<Country />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
